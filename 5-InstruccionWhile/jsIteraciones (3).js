6d0647d7d0cfdeddd9b5eddbfc25f88f652cbcf6
Helalubo <alejandrodemoraiz@gmail.com>
2019-02-22 17:43:37 -0300

    intentando hacer adivina el numero  v2.

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,19 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+  	var clave = prompt("ingrese el número clave.");
+
+
+  		while(clave !="utn750")
+  		{
+  		alert("clave incorrecta");
+  		clave=prompt("ingrese la clave nuevamente");
+  		}
+
+
+  alert("bienvenidos");
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -1,5 +1,5 @@
 /*En esta oportunidad el juego evaluará tus
- aptitudes a partir de la cantidad de intentos, 
+ aptitudes a partir de la cantidad de intentos,
  por lo cual se informará lo siguiente:
 1° intento: “usted es un Psíquico”.
 2° intento: “excelente percepción”.
@@ -9,20 +9,69 @@
 Desde  6 Intentos hasta 10:”falta técnica”
 Más de 10 intentos: “afortunado en el amor!!”.*/
 
-var numeroSecreto; 
+var numeroSecreto;
 var contadorIntentos;
-
+var numero;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
+	numeroSecreto=Math.floor(Math.random()*(100))+1;
+	alert("numero secreto puesto toque verificar");
 
 }
+var contadorIntentos;
+contadorIntentos=0
 
 function verificar()
 {
-	
-	
+	numero=document.getElementById('numero').value;
 
+contadorIntentos+1
+	if(numero==numeroSecreto){
+	switch (contadorIntentos) {
+	case "1":
+					mensaje = "usted es un Psíquico";
+			break;
+			case "2":
+				mensaje = "excelente percepción”"
+				break;
+				case "3":
+					mensaje = "esto es suerte";
+					break;
+					case "4":
+					mensaje = "Excelente técnica";
+					break;
+					case "5":
+					mensaje = "usted está en la media";
+					break;
+					case "6":
+					case "7":
+					case "8" :
+					case "9" :
+					case "10":
+					mensaje = "falta técnica";
+								break;
+		default: mensaje = "afortunado en el amor!!"
+		break;
+
+	}
+	if(numeroSecreto<numero)
+	{
+		mensaje = "sobra";
+	}
+else{
+		mensaje = " falta "
+	}
+	document.getElementById('intentos').value=contadorIntentos;
+
+	alert(mensaje);
+
+
+
+
+
+
+
+
+}
 }
