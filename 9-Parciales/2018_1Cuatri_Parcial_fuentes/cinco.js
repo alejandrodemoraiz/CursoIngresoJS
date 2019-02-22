2baab6339f9bf60c18db20ac36a4a53b5e6dea12
Helalubo <alejandrodemoraiz@gmail.com>
2019-02-22 05:48:53 -0300

    ejercicios de parcial.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+var planeta;
+planeta = prompt(" planeta del sistema solar:")
+switch (planeta) {
+  case "mercurio":
+  case "venus":
+    mensaje = "acá hace más calor"
+    break;
+  case "tierra":
+  mensaje = "Aca vivimos"
+  break;
+  case "marte":
+  case "jupiter":
+  case "saturno":
+  case "urano":
+  case "neptuno":
+  break;
+  mensaje = "Aca hace mas frio";
+  default: alert(" ese planeta no esta en nuestro sistema solar");
+  break;
 
+}
+alert(mensaje);
 }
