c555958be2e2ba212403a02d31ddfa4a625e256a
Helalubo <alejandrodemoraiz@gmail.com>
2019-02-28 17:44:27 -0300

    parcial 8, falta arreglar, no me deja pedir mas de un dato;

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,14 +1,100 @@
 function mostrar()
 {
-var letra;
-var numeroIngresado;
-var respuesta
+var numeroElegido;
+var letraElegida;
+var contador;
+var respuesta;
+var contadorNumerosPares;
+var contadorNumerosImpares;
+var contadorceros;
+var contadorPositivos;
+var acumuladorPositivos;
+var promedioPositivos;
+var acumuladorNegativo;
+var numeroMaximo;
+var numeroMinimo;
+var letraDeNumeroMaximo;
+var letraDeNumerominimo;
+
 respuesta = "si";
-while (respuesta == "si") {
-  numeroIngresado = prompt(" ingrese un numero ");
-  while( numeroIngresado < "-100 " || numeroIngresado > "100" );
-  numeroIngresado = prompt(" ERROR! ingrese un numero ");
-  
+contador = 0;
+contadorNumerosPares = 0;
+contadorNumerosImpares = 0;
+contadorceros = 0;
+contadorPositivos =0;
+acumuladorPositivos =0;
+acumuladorNegativo = 0;
+promediopositivos= 0;
+promedio= 0;
+
+
+while (respuesta == "si")
+{          contador++;
+          letraElegida = prompt("Ingrese una letra");
+          numeroElegido = prompt(" ingrese un numero");
+          numeroElegido = parseInt(numeroElegido);
+          while (numeroElegido < -100 || numeroElegido > 100)
+                      {
+                numeroElegido = prompt(" ERROR! ingrese un numero")
+                      numeroElegido = parseInt(numeroElegido);
+                      }
+          respuesta = prompt(" desea continuar? de ser asi escriba ==> si ");
+
+if (numeroElegido %2 == 0)
+{
+ contadorNumerosPares = contadorNumerosPares + 1;
+
+}else {
+  contadorNumerosImpares = contadorNumerosImpares + 1;
 
 }
+if (numeroElegido == 0) {
+  contadorceros = contadorceros + 1;
+
+}
+if (numeroElegido > 0) {
+  contadorPositivos = contadorPositivos + 1;
+  acumuladorPositivos= acumuladorPositivos + numeroElegido;
+
+}
+else {
+    acumuladorNegativo = acumuladorNegativo + numeroElegido;
+}
+
+if (contador == 1) {
+  maximo = numeroElegido;
+  letraDeNumeroMaximo = letraElegida;
+  minimo = numeroElegido;
+  letraDeNumerominimo = letraElegida;
+
+
+}else {
+        if (numeroElegido > maximo)
+        {
+          maximo = numeroElegido;
+          letraDeNumeroMaximo = letraElegida;
+
+        }
+        if (numeroElegido < minimo)
+        {
+          minimo = numeroElegido;
+          letraDeNumerominimo = letraElegida;
+
+
+        }
+
+    }
+}
+
+
+
+
+promedio = acumuladorPositivos / contadorPositivos;
+
+document.write(" la cantidad de pares es de :" + contadorNumerosPares + "cantidad de numeros impares:" + contadorNumerosImpares +
+"la cantidad de 0 es de :" + contadorceros+ "el promedio de los positivos es de:" + promedio+
+"la suma de los numeros negativos es de:" +acumuladorNegativo +
+" el numero y la letra maximos si " + maximo + letraDeNumeroMaximo + " y el numero minimo es " + minimo + "." );
+
+
 }
