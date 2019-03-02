caee52eb969a21cef04c8130641b372b35749b3e
Alejandro De Moraiiz <47877440+alejandrodemoraiz@users.noreply.github.com>
2019-03-01 22:00:09 -0300

    recu clases ejercicios
    recu parciales ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,88 @@
 function mostrar()
 {
+	var pais;
+	var superficie;
+	var respuesta;
+	var contador;
+	var cantidadPaisesSuperficieImpa;
+	var cantidadPaisesSuperficiepar;
+	var cantidadPaisesSuperficieMenora100;
+	var cantidadPaisesSuperficieIguala100;
+	var primerPaisSuperando100DeSuperficie;
+	var contadorMayor100;
+	var cantidadDeSuperficie;
+	var PaisConMenosSuperficie;
+	var nombreDePaisConMenosSuperficie;
+	var promedio;
+	primerPaisSuperando100DeSuperficie = "bandera";
+	contador = 0;
+	respuesta = "si";
+	cantidadPaisesSuperficieImpa =0;
+	cantidadPaisesSuperficiepar=0;
+	cantidadPaisesSuperficieMenora100 = 0;
+	cantidadPaisesSuperficieIguala100 = 0;
+	contadorMayor100 = 0;
+	cantidadDeSuperficie =0;
+
+	while(respuesta == "si")
+	{	
+		contador++;
+		pais = prompt("Ingrese un pais" + contador + "#");
+		superficie = prompt("Ingrese superficie"+ contador + "#");
+		superficie =parseInt(superficie);
+		while(superficie > 5000)
+		{
+			superficie = prompt("Ingrese una superficie valida, menor a 5000");
+		}	superficie =parseInt(superficie);
+
+		respuesta = prompt( "desea seguir ingresando paises y su superficie? ==> si , para continuar");
+
+ 		cantidadDeSuperficie = cantidadDeSuperficie + superficie;
+	
+   if (superficie%2 == 0) {
+   	   cantidadPaisesSuperficiepar = cantidadPaisesSuperficiepar + 1;
+   							}
+
+    if(superficie%2 != 0){
+    	cantidadPaisesSuperficieImpa++;
+
+    }
+   
+   if (superficie < 100) 
+   {
+   	cantidadPaisesSuperficieMenora100 = cantidadPaisesSuperficieMenora100 + 1;
+   }
+   	if (superficie == 100)
+   	{
+   		cantidadPaisesSuperficieIguala100 =cantidadPaisesSuperficieIguala100 + 1;
+   	}
+ 	if ( superficie > 100)
+ 	{       
+ 			contadorMayor100 ++;
+ 			if (contadorMayor100 == 1) {
+ 				primerPaisSuperando100DeSuperficie = pais;
+ 			}
+ 			if (contador == 1) {
+ 				PaisConMenosSuperficie = superficie;
+ 				nombreDePaisConMenosSuperficie = pais;
+ 			}
+ 			else{
+ 				if (superficie < PaisConMenosSuperficie) 
+ 				{
+ 					PaisConMenosSuperficie = superficie;
+ 					nombreDePaisConMenosSuperficie = pais;
+ 				}
+ 			}
+
+ 	}
+}
+promedio = cantidadDeSuperficie / contador;
+document.write(" la cantidad de paises con superficie impar es de " +cantidadPaisesSuperficieImpa + "<br>" +
+  "la cantidad de paises con superficie menor a 100 es de " +cantidadPaisesSuperficieMenora100 + "<br>"+
+  " cantidad de paises con superficie igual a 100 es de  " +cantidadPaisesSuperficieIguala100 + "<br>"+
+  " El nombre del primer pais que supero los 100 de superficie es " +primerPaisSuperando100DeSuperficie+ "<br>"+
+  " promedio de kilometros ingresados es " + promedio + "<br>" +
+  "el nombre del pais con menos territorio es " + nombreDePaisConMenosSuperficie + " ;) ");
 
 }
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,71 @@
 function mostrar()
 {
+	var kilometrosIngresados;
+	var tipoDeCombustible;
+	var contador;
+	var totalkilometros;
+	var promedio;
+	var velocidadMasBaja;
+	var velocidadMasBajaCombustible;
+	var cantidadCombistiblesLiquidos;
+	var velocidadMasAltaCombustiblesSolidos;
+	contador = 0;
+	totalkilometros = 0;
+	cantidadCombistiblesLiquidos = 0;
+
+	while(contador <5)
+		{   
+			contador++
+		kilometrosIngresados = prompt("Ingrese kilometos " + contador + "#");
+		kilometrosIngresados = parseInt(kilometrosIngresados);
+		while(kilometrosIngresados < 0 || kilometrosIngresados > 250)
+		{
+			kilometrosIngresados = prompt("ERROR! Ingrese kilometos "+ contador + "#");
+		kilometrosIngresados = parseInt(kilometrosIngresados);
+		}
+
+		tipoDeCombustible = prompt(" tipo de combustible, ingrese =>s para solido o => l para liquido " + contador + "#");
+ 		while(tipoDeCombustible != "l" && tipoDeCombustible != "s")
+ 		{
+ 			tipoDeCombustible = prompt(" ERROR! tipo de combustible, ingrese =>s para solido o => l para liquido " + contador + "#");
+ 		}
+	
+ 		totalkilometros = totalkilometros + kilometrosIngresados;
+
+ 		if (contador == 1) {
+ 			velocidadMasBaja = kilometrosIngresados;
+ 			velocidadMasBajaCombustible = tipoDeCombustible;
+ 							}
+ 		else{
+              if (kilometrosIngresados < velocidadMasBaja) 
+              {
+              		velocidadMasBaja = kilometrosIngresados;
+ 					velocidadMasBajaCombustible = tipoDeCombustible;
+              }
+ 			}
+
+ 			if (tipoDeCombustible == "l" && kilometrosIngresados > 100) 
+ 			{
+  				cantidadCombistiblesLiquidos = cantidadCombistiblesLiquidos + 1;
+ 			}
+ 				if (tipoDeCombustible == "s")
+ 				{
+ 					if (contador == 1) 
+ 					{
+ 						velocidadMasAltaCombustiblesSolidos = kilometrosIngresados;
+
+ 					}else{
+ 						if (kilometrosIngresados > velocidadMasAltaCombustiblesSolidos) 
+ 						{
+ 							 velocidadMasAltaCombustiblesSolidos = kilometrosIngresados;
+ 						}
+ 				}
+	}
+}
+  promedio = totalkilometros/contador;
+  alert("el promedio de las velocidades es de " + promedio+ "k/h");
+  alert(" la velocidad mas baja es de " +velocidadMasBaja + "y su tipo de combustible es " +velocidadMasBajaCombustible);
+ alert(" la cantidad vehiculos con combustible liquido que supera a 100km es " + cantidadCombistiblesLiquidos);
+alert(" la velocidad mas alta de los combustibles solidos es " + velocidadMasAltaCombustiblesSolidos+ "k/h");
 
 }
