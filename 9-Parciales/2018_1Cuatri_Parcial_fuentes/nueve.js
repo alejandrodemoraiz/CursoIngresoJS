5c4b0749b649b556668c1ae921656e7e593898fc
Helalubo <alejandrodemoraiz@gmail.com>
2019-03-06 15:11:59 -0300

    parcial 9 re hecho para practica

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,93 +1,91 @@
 function mostrar()
 {
-  var marcaIngresada;
-  var pesoIngresado;
-  var temperaturaIngresada;
+
+  var marca;
+  var precio;
+  var peso;
+  var temperatura;
   var respuesta;
   var contador;
   var cantidadDeTemperaturasPares;
   var productoMasPesado;
+  var productoMenosPesado;
   var productoMasPesadoMarca;
-  var cantidadDeProductosA0;
-  var pesoTotalProductos;
-  var promedio;
-  var pesoMaximo;
-  var pesoMinimo;
-  cantidadDeTemperaturasPares = 0;
-  contador = 0;
-  cantidadDeProductosA0 = 0;
-  pesoTotalProductos = 0;
+  var cantidadProductorAMenos0;
+  var promedioDeTodoslosProductos;
+  var pesoTotal;
+
+  //
   respuesta = "si";
+  contador = 0;
+  cantidadDeTemperaturasPares = 0;
+  cantidadProductorAMenos0 = 0;
+  pesoTotal = 0;
 
   while (respuesta == "si")
-  {
-    contador = contador + 1;
-    marcaIngresada = prompt("Ingrese la marca del producto " +contador+"#");
-
-    pesoIngresado = prompt("Ingrese el peso del producto " +contador+"#");
-    pesoIngresado = parseInt(pesoIngresado);
-    pesoTotalProductos = pesoTotalProductos + pesoIngresado;
-    while ( pesoIngresado < 1 || pesoIngresado > 100)
-          {
-      pesoIngresado = prompt("ERROR! Ingrese el peso del producto " +contador+"#");
-    pesoIngresado = parseInt(pesoIngresado);
-
-          }
-    temperaturaIngresada = prompt(" ingrese la temperatura de almacenamiento "+contador+"#")
-    temperaturaIngresada = parseInt(temperaturaIngresada);
-    while (temperaturaIngresada < -30 || temperaturaIngresada > 30)
     {
-      temperaturaIngresada = prompt(" ERROR! ingrese la temperatura de almacenamiento "+contador+"#")
-      temperaturaIngresada = parseInt(temperaturaIngresada);
+      contador = contador + 1;
+    marca = prompt(" ingrese la marca del producto" +contador+ "# ");
 
-    } respuesta = prompt ("para continuar ingresando productos escriba ===> si ");
-    if (temperaturaIngresada %2 == 0)
+    peso =prompt(" ingrese el peso del producto" +contador+ "# ");
+    peso = parseInt(peso);
+    while (peso < 1 || peso > 100 )
+    {
+      peso =prompt(" ERROR! ingrese el peso del producto" +contador+ "# ");
+      peso = parseInt(peso);
+
+    }
+
+    precio = prompt(" ingrese el precio del producto" +contador+ "# ");
+
+    temperatura = prompt(" ingrese la temperatura de almacenamiento del producto" +contador+ "# ");
+    temperatura = parseInt(temperatura);
+    while (temperatura < -30 || temperatura > 30)
+    {
+      temperatura = prompt("ERROR! ingrese la temperatura de almacenamiento del producto" +contador+ "# ");
+      temperatura = parseInt(temperatura);
+    }
+
+    respuesta = prompt(" si desea continuar ingresando productos y sus caracteristicas escriba --> si");
+
+      if (temperatura% 2 == 0)
+      {
+       cantidadDeTemperaturasPares = cantidadDeTemperaturasPares + 1 ;
+      }
+      if (contador == 1)
+      {
+          productoMasPesado = peso;
+          productoMasPesadoMarca = marca;
+          productoMenosPesado = peso;
+
+      }
+      else
+      {
+        if (peso > productoMasPesado)
         {
-          cantidadDeTemperaturasPares = cantidadDeTemperaturasPares + 1;
-
+          productoMasPesado = peso;
+          productoMasPesadoMarca = marca;
         }
-
-        if (contador ==1)
+        if(peso < productoMenosPesado)
         {
-          productoMasPesado = pesoIngresado;
-          productoMasPesadoMarca = marcaIngresada;
+                productoMenosPesado = peso;
         }
-        else {
-          if (productoMasPesado > pesoIngresado) {
+      }
+if (temperatura < 0)
+    {
+      cantidadProductorAMenos0 = cantidadProductorAMenos0 + 1;
+    }
 
-            productoMasPesado = pesoIngresado;
-            productoMasPesadoMarca = marcaIngresada;
-          }
-        }
+    pesoTotal = pesoTotal + peso;
 
-          if (temperaturaIngresada < 0) {
-            cantidadDeProductosA0 = cantidadDeProductosA0 + 1;
-          }
 
-   if (contador == 1)
-   {
-     pesoMaximo = pesoIngresado;
-     pesoMinimo = pesoIngresado;
 
-   }else {
-     if (pesoIngresado> pesoMaximo)
-     {
-       pesoMaximo = pesoIngresado;
+    }
+promedioDeTodoslosProductos = pesoTotal / contador;
 
-     }if (pesoIngresado< pesoMinimo)
-     {
-          pesoMinimo = pesoIngresado;
-     }
-
-     }
-   }
-
-promedio = pesoTotalProductos/ contador;
-
-document.write( " la cantidad de temperaturas pares es de " +cantidadDeTemperaturasPares + "<br>" +
-"la marca del producto mas pesado es " + productoMasPesadoMarca + "<br>" +
-"la cantidad de productos que se conservan a menos 0 grados es de " +cantidadDeProductosA0 + "<br>" +
-"el promedio de todos los productos es de " +promedio + "<br>" +
-" el peso maximo es de " +pesoMaximo + " y el minimo es de " +pesoMinimo + ".");
-
+ document.write("la cantidad de temperaturas pares es de " +cantidadDeTemperaturasPares + "<br>" +
+"la marca del producto mas pesado es " + productoMasPesadoMarca+ "<br>" +
+" La cantidad de productos que se conservan a menos de 0 grados " + cantidadProductorAMenos0 +  "<br>" +
+" el promedio del peso de todos los productos es de " + promedioDeTodoslosProductos + "<br>" +
+"el peso maximo es de " + productoMasPesado + " y el minimo es de " + productoMenosPesado+ " .");
 }
